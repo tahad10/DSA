30388c6c38f793507696d61853648872c27d3814
tahad10 <148988143+tahad10@users.noreply.github.com>
2024-04-22 15:02:22 +0200

    Add files via upload

---
+++ main/java/de/unistuttgart/dsass2024/ex01/p5/Sorter.java
@@ -0,0 +1,41 @@
+package de.unistuttgart.dsass2024.ex01.p5;
+
+public class Sorter {
+
+    /**
+     * Performs selection sort on provided list, works directly on list object,
+     * hence no return value
+     * 
+     * @param <T>  The type of list element
+     * @param list List to apply the sorting to; unsorted list at first, sorted list
+     *             in the end
+     */
+    public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
+
+    }
+
+    /**
+     * Performs insertion sort on provided list, works directly on list object,
+     * hence no return value
+     * 
+     * @param <T>  The type of list element
+     * @param list List to apply the sorting to; unsorted list at first, sorted list
+     *             in the end
+     */
+    public static <T extends Comparable<T>> void insertionSort(ISimpleList<T> list) {
+
+    }
+
+    /**
+     * Performs bubble sort on provided list, works directly on list object, hence
+     * no return value
+     * 
+     * @param <T>  The type of list element
+     * @param list List to apply the sorting to; unsorted list at first, sorted list
+     *             in the end
+     */
+    public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
+
+    }
+}
+//Test

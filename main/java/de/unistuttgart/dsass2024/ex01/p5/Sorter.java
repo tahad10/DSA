ca53e84e79d7401d8ab35574b1aba94be40d8e4e
tahad10 <148988143+tahad10@users.noreply.github.com>
2024-04-27 14:35:49 +0200

    Add files via upload

--- main/java/de/unistuttgart/dsass2024/ex01/p5/Sorter.java
+++ main/java/de/unistuttgart/dsass2024/ex01/p5/Sorter.java
@@ -11,7 +11,16 @@ public class Sorter {
      *             in the end
      */
     public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
-
+        int maxIndex;
+        for (int i = 0; i < list.getSize() - 1; i++){
+            maxIndex = i;
+            for (int j = i + 1; j < list.getSize(); j++){
+                if (list.getElement(j).compareTo(list.getElement(maxIndex)) > 0){
+                    maxIndex = j;
+                }
+            }
+            list.swapElements(i, maxIndex);
+        }
     }
 
     /**
@@ -23,7 +32,15 @@ public class Sorter {
      *             in the end
      */
     public static <T extends Comparable<T>> void insertionSort(ISimpleList<T> list) {
-
+      for (int i = 1; i < list.getSize(); i++){
+          T key = list.getElement(i);
+          int j = i -1;
+          while (j >= 0 && list.getElement(j).compareTo(key) > 0){
+              list.swapElements(j + 1, j);
+              j--;
+          }
+          list.swapElements(j + 1, i);
+      }
     }
 
     /**
@@ -35,7 +52,12 @@ public class Sorter {
      *             in the end
      */
     public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
-
+        for (int i = 0; i < list.getSize() - 1; i++){
+            for (int j = 0; j < list.getSize() - i - 1; j++){
+                if (list.getElement(j).compareTo(list.getElement(j + 1)) < 0){
+                    list.swapElements(j, j + 1);
+                }
+            }
+        }
     }
 }
-//Test
